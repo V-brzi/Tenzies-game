5ce64c7df91bde6de4198a6c503a3aab196d82b6
V-brzi <vatro.brzi@gmail.com>
2022-09-18 15:48:06 +0200

    update

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -7,7 +7,7 @@ export default function Die(props){
             <img className={props.isHeld ? "die-img held" : "die-img"}
             id={props.id} 
             onClick={(event) => props.holdDiceNumber(event)} 
-            src={process.env.PUBLIC_URL + `/img/Dice-img${props.value}.png`} 
+            src={`/Dice-img${props.value}.png`} 
             alt="die" />
         </div>
     )
